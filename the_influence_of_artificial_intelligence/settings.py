093956c4333e74cba2b461f6385a887c6c603284
REDDY NAGA HARSHITHAMBA <harshithareddysv7@gmail.com>
2026-07-02 17:00:25 +0530

    Fix ALLOWED_HOSTS for Render

--- the_influence_of_artificial_intelligence/settings.py
+++ the_influence_of_artificial_intelligence/settings.py
@@ -14,7 +14,11 @@ SECRET_KEY = 'm+1edl5m-5@u9u!b8-=4-4mq&o1%agco2xpl8c!7sn7!eowjk#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "ai-for-smart-citys-e-governence.onrender.com",
+    "127.0.0.1",
+    "localhost",
+]
 
 
 # Application definition
